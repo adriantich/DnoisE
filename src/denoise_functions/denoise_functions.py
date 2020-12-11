15aac7cbd50213380784ae5d6274658d58999682
AdriAntich <adriantich@gmail.com>
2020-12-11 17:15:42 +0100

    importing_variables

--- src/denoise_functions/denoise_functions.py
+++ src/denoise_functions/denoise_functions.py
@@ -4,31 +4,14 @@ import Levenshtein as lv
 import json
 from ast import literal_eval
 import os
-# import shutil
+import getopt
+import sys
 
-# not_possibleMothers = []
-# possibleMothers = []
-# data_inicial = []
-# runned_list = []
-# alpha = []
-# min_mother = []
-# Ad1 = []
-# Ad2 = []
-# Ad3 = []
-# MOTUfile = ""
-# good_seq = []
-# output_info = []
-# denoised_d_output = []
-# denoised_ratio_output = []
-# denoised_ratio_d_output = []
-# entropy = []
-# cores = []
-# MOTUoutfile = ""
 
 class denoise_functions:
     not_possibleMothers = 0
     possibleMothers = 0
-    data_inicial = pd.DataFrame()
+    data_initial = pd.DataFrame()
     runned_list = pd.DataFrame()
     alpha = 5
     min_mother = 20
@@ -61,14 +44,197 @@ class denoise_functions:
     q8 = 0
     q9 = 0
     q10 = 0
-    inicial_pos = 2
+    initial_pos = 2
     justcount = True
     abund_col_names = []
     first_col_names = []
     fasta_output = True
+    fasta = True
+    sep = '\t'
+    end = 1
+    start = 1
 
     def __init__(self):
         print("starting to denoise")
+
+    def read_parameters(self, argument_list):
+        short_options = "hi:o:P:f:F:c:s:z:n:a:q:p:e:y:"
+        long_options = ["help", "input=", "output=", "part=", "fasta_input=", "fasta_output=", "cores=",
+                        "start_sample_cols=",
+                        "end_sample_cols=", "count_name=", "alpha=", "sequence=", "separation=", "entropy=",
+                        "entropy_influence="]
+        try:
+            arguments, values = getopt.getopt(argument_list, short_options, long_options)
+        except getopt.error as err:
+            # Output error, and return with an error code
+            print(str(err))
+            sys.exit(2)
+        for current_argument, current_value in arguments:
+            if current_argument in ("-h", "--help"):
+                print("Displaying help\n"
+                      " -h --help Display help\n"
+                      " -i --input input file path\n"
+                      " -o --output common output files path\n"
+                      " -P --part Denoise can be runned per parts, part 1 run just possible mothers and part 2 run the others\n"
+                      "                     In part 1 a directory as database is named as --output\n"
+                      "                     In part 2 (parallellitzable) this directory is removed and outputs are printed\n"
+                      "                         Part 2 requires --input, --output and --cores if necessary\n"
+                      " -f --fasta_input logical, if T, fasta file as input, if F (default) .csv as input\n"
+                      " -F --fasta_output logical, if T, fasta file as input, if F (default) .csv as input\n"
+                      " -c --cores number of cores, 1 by default\n"
+                      " -s --start_sample_cols first sample column (1 == 1st col) if not given, just total read count expected\n"
+                      " -z --end_sample_cols first sample column (1 == 1st col) if not given, just total read count expected\n"
+                      " -n --count_name count name column (count/reads/size..) 'count' by default\n"
+                      " -a --alpha alpha value, 5 by default\n"
+                      " -q --sequence sequence column name, 'sequence' by default\n"
+                      " -p --sep separation 1='\t'\n"
+                      "                     2=','\n"
+                      "                     3=';'\n"
+                      " -e --entropy entropy of different positions [0.4298,0.1833,0.9256] by default\n"
+                      " -y --entropy_influence logical, if T, Ad correction parameter is used. When this happens, only "
+                      "sequences with mode length are computed")
+                exit()
+            elif current_argument in ("-i", "--input"):
+                print("Denoising %s file" % current_value)
+                self.MOTUfile = current_value
+                arg_i = True
+            elif current_argument in ("-o", "--output"):
+                print("Output files will be named %s*" % current_value)
+                self.MOTUoutfile = current_value
+                arg_o = True
+            elif current_argument in ("-P", "--Part"):
+                print("Part %s running" % current_value)
+                self.part = int(current_value)
+                self.parted = True
+                arg_P = True
+            elif current_argument in ("-c", "--cores"):
+                print("Running with %s cores" % current_value)
+                self.cores = int(current_value)
+                arg_c = True
+            elif current_argument in ("-f", "--fasta_input"):
+                if current_value == "T":
+                    self.fasta = True
+                else:
+                    self.fasta = False
+                arg_f = True
+                print("Fasta file: %s" % self.fasta)
+            elif current_argument in ("-F", "--fasta_output"):
+                if current_value == "T":
+                    self.fasta_output = True
+                else:
+                    self.fasta_output = False
+                arg_F = True
+                print("Fasta output file: %s" % self.fasta_output)
+            elif current_argument in ("-s", "--start_sample_cols"):
+                self.start = int(current_value)
+                arg_s = True
+                print("Abundant cols starts in: %s" % current_value)
+            elif current_argument in ("-z", "--end_sample_cols"):
+                self.end = int(current_value)
+                arg_z = True
+                print("Abundant cols ends in: %s" % current_value)
+            elif current_argument in ("-n", "--count_name"):
+                self.count = current_value
+                arg_n = True
+                print("Count col name: %s" % current_value)
+            elif current_argument in ("-a", "--alpha"):
+                self.alpha = int(current_value)
+                arg_a = True
+                print("Alpha: %s" % current_value)
+            elif current_argument in ("-q", "--sequence"):
+                self.seq = current_value
+                arg_q = True
+                print("Seq: %s" % current_value)
+            elif current_argument in ("-p", "--sep"):
+                print(current_value)
+                if current_value == "1":
+                    self.sep = '\t'
+                if current_value == "2":
+                    self.sep = ','
+                if current_value == "3":
+                    self.sep = ';'
+                arg_p = True
+                print("Sep: %s" % self.sep)
+            elif current_argument in ("-e", "--entropy"):
+                E1, E2, E3 = current_value.split(",")
+                print(str("E1: " + E1 + " \nE2: " + E2 + " \nE3: " + E3))
+                E1 = float(E1)
+                E2 = float(E2)
+                E3 = float(E3)
+                arg_e = True
+            elif current_argument in ("-y", "--entropy_influence"):
+                if current_value == "T":
+                    self.entropy = True
+                else:
+                    self.entropy = False
+                arg_y = True
+                print("Is entropy taken into account: %s" % self.entropy)
+
+        if 'arg_i' not in locals():
+            print("Err: input file needed")
+            exit()
+        if 'arg_o' not in locals():
+            print("Err: output path needed")
+            exit()
+        if 'arg_P' not in locals():
+            print("Running all at once")
+            self.parted = False
+            self.part = 1
+        if self.part == 1:
+            if 'arg_c' not in locals():
+                print("cores not given, 1 core by default")
+                self.cores = 1
+            if 'arg_f' not in locals():
+                # de moment per a la opcio amb fasta no hi ha la opció de posar samples
+                print("by default, fasta file expected")
+                self.fasta = True
+                self.count = "count"
+                self.justcount = True
+                if 'arg_F' not in locals():
+                    self.fasta_output = True
+                    print("by default, fasta output")
+            else:
+                if "arg_n" not in locals():
+                    print("count_name not given, 'count' by default")
+                    self.count = "count"
+                if "arg_s" not in locals():
+                    if "arg_z" not in locals():
+                        print("start and end not given, no samples in file")
+                        abund_col_names = [self.count]
+                        self.justcount = True
+                    else:
+                        print("Err: end given but no start")
+                        exit()
+                elif "arg_z" not in locals():
+                    print("Err: start given but no end")
+                    exit()
+                else:
+                    self.justcount = False
+                    if self.fasta_output:
+                        print('WARNING!!!! .csv output when samples information is given')
+                    self.fasta_output = False
+                    print("output as .csv")
+                if "arg_q" not in locals():
+                    print("sequence not given, 'sequence' by default")
+                    self.seq = 'sequence'
+                if "arg_p" not in locals():
+                    print("Separation not given, '\t' by default")
+                    self.sep = '\t'
+            if "arg_a" not in locals():
+                print("alpha not given, 5 by default")
+                self.alpha = 5
+            if "arg_y" not in locals():
+                self.entropy = False
+                print("Ad correction not applied")
+                self.Ad1, self.Ad2, self.Ad3 = (1, 1, 1)
+            else:
+                if "arg_e" not in locals():
+                    E1, E2, E3 = (0.4298, 0.1833, 0.9256)
+                    print("Entropy set as 0.4298, 0.1833 and 0.9256 by default")
+                # defining Ad correction factor taking into account Entropy
+                self.Ad1 = E1 / (E1 + E2 + E3) * 3
+                self.Ad2 = E2 / (E1 + E2 + E3) * 3
+                self.Ad3 = E3 / (E1 + E2 + E3) * 3
 
     def quartiles_runned(self):
         q = self.not_possibleMothers - 1 - self.possibleMothers
@@ -85,9 +251,9 @@ class denoise_functions:
         self.q10 = self.not_possibleMothers - 1
 
     def denoising(self, pos):
-        pD = self.data_inicial.loc[pos, 'id']
-        pDseq = self.data_inicial.loc[pos, self.seq]
-        pDabund = self.data_inicial.loc[pos, self.count]
+        pD = self.data_initial.loc[pos, 'id']
+        pDseq = self.data_initial.loc[pos, self.seq]
+        pDabund = self.data_initial.loc[pos, self.count]
         if pos == 0:
             info = {'daughter': pD, 'mother_d': None, 'd': None,
                     'mother_ratio': None, 'ratio': None,
@@ -109,8 +275,8 @@ class denoise_functions:
                 continue
             pM = self.runned_list.iloc[a, 0]
             pMpos = a
-            pMseq = self.data_inicial.loc[pMpos, self.seq]
-            pMabund = self.data_inicial.loc[pMpos, self.count]
+            pMseq = self.data_initial.loc[pMpos, self.seq]
+            pMabund = self.data_initial.loc[pMpos, self.count]
             # obtain ratio ---> total_reads pD / total_reads pM
             b_ratio = pDabund / pMabund
             # if ratio is less than minimum (1/64)
@@ -199,9 +365,9 @@ class denoise_functions:
         elif pos == self.q10:
             print('100% aprox')
 
-        pD = self.data_inicial.loc[pos, 'id']
-        pDseq = self.data_inicial.loc[pos, self.seq]
-        pDabund = self.data_inicial.loc[pos, self.count]
+        pD = self.data_initial.loc[pos, 'id']
+        pDseq = self.data_initial.loc[pos, self.seq]
+        pDabund = self.data_initial.loc[pos, self.count]
         position = self.runned_list.shape[0]
         # create void list for pM info ---> Motherslist (Ml)
         Ml = pd.DataFrame(columns=['pM', 'pMpos', 'pD', 'ratio', 'd', 'xavier_criteria'])
@@ -213,8 +379,8 @@ class denoise_functions:
                 continue
             pM = self.runned_list.iloc[a, 0]
             pMpos = a
-            pMseq = self.data_inicial.loc[pMpos, self.seq]
-            pMabund = self.data_inicial.loc[pMpos, self.count]
+            pMseq = self.data_initial.loc[pMpos, self.seq]
+            pMabund = self.data_initial.loc[pMpos, self.count]
             # obtain ratio ---> total_reads pD / total_reads pM
             b_ratio = pDabund / pMabund
             # if ratio is less than minimum (1/64)
@@ -264,7 +430,7 @@ class denoise_functions:
             # exist
         else:  # it is a daughter
             # print pD name to each pM depending on different criteria
-            # pDinfo = np.transpose(pd.DataFrame(self.data_inicial.loc[pos, abund_col_names]))
+            # pDinfo = np.transpose(pd.DataFrame(self.data_initial.loc[pos, abund_col_names]))
             pM_d = Ml.loc[(Ml['d'] == min(Ml.loc[:, 'd'])), 'pM'][0]
             pM_ratio = Ml.loc[(Ml['ratio'] == min(Ml.loc[:, 'ratio'])), 'pM'][0]
             if type(pM_ratio) is not str:
@@ -282,7 +448,7 @@ class denoise_functions:
         difcount = 0
         for i, o in enumerate(seq1):
             if o != seq2[i]:
-                dif_position = (i + self.inicial_pos) % 3
+                dif_position = (i + self.initial_pos) % 3
                 if dif_position == 1:
                     difcount += self.Ad1
                 elif dif_position == 2:
@@ -292,9 +458,9 @@ class denoise_functions:
         return difcount
 
     def denoising_Adcorrected(self, pos):
-        pD = self.data_inicial.loc[pos, 'id']
-        pDseq = self.data_inicial.loc[pos, self.seq]
-        pDabund = self.data_inicial.loc[pos, self.count]
+        pD = self.data_initial.loc[pos, 'id']
+        pDseq = self.data_initial.loc[pos, self.seq]
+        pDabund = self.data_initial.loc[pos, self.count]
 
         if pos == 0:
             info = {'daughter': pD, 'mother_d': None, 'd': None,
@@ -317,8 +483,8 @@ class denoise_functions:
                 continue
             pM = self.runned_list.iloc[a, 0]
             pMpos = a
-            pMseq = self.data_inicial.loc[pMpos, self.seq]
-            pMabund = self.data_inicial.loc[pMpos, self.count]
+            pMseq = self.data_initial.loc[pMpos, self.seq]
+            pMabund = self.data_initial.loc[pMpos, self.count]
             # obtain ratio ---> total_reads pD / total_reads pM
             b_ratio = pDabund / pMabund
             # if ratio is less than minimum (1/64)
@@ -406,9 +572,9 @@ class denoise_functions:
         elif pos == self.q10:
             print('100% aprox')
 
-        pD = self.data_inicial.loc[pos, 'id']
-        pDseq = self.data_inicial.loc[pos, self.seq]
-        pDabund = self.data_inicial.loc[pos, self.count]
+        pD = self.data_initial.loc[pos, 'id']
+        pDseq = self.data_initial.loc[pos, self.seq]
+        pDabund = self.data_initial.loc[pos, self.count]
         position = self.runned_list.shape[0]
         # create void list for pM info ---> Motherslist (Ml)
         Ml = pd.DataFrame(columns=['pM', 'pMpos', 'pD', 'ratio', 'd', 'xavier_criteria'])
@@ -420,8 +586,8 @@ class denoise_functions:
                 continue
             pM = self.runned_list.iloc[a, 0]
             pMpos = a
-            pMseq = self.data_inicial.loc[pMpos, self.seq]
-            pMabund = self.data_inicial.loc[pMpos, self.count]
+            pMseq = self.data_initial.loc[pMpos, self.seq]
+            pMabund = self.data_initial.loc[pMpos, self.count]
             # obtain ratio ---> total_reads pD / total_reads pM
             b_ratio = pDabund / pMabund
             # if ratio is less than minimum (1/64)
@@ -471,7 +637,7 @@ class denoise_functions:
             # exist
         else:  # it is a daughter
             # print pD name to each pM depending on different criteria
-            # pDinfo = np.transpose(pd.DataFrame(self.data_inicial.loc[pos, abund_col_names]))
+            # pDinfo = np.transpose(pd.DataFrame(self.data_initial.loc[pos, abund_col_names]))
             pM_d = Ml.loc[(Ml['d'] == min(Ml.loc[:, 'd'])), 'pM'][0]
             pM_ratio = Ml.loc[(Ml['ratio'] == min(Ml.loc[:, 'ratio'])), 'pM'][0]
             if type(pM_ratio) is not str:
@@ -518,8 +684,8 @@ class denoise_functions:
         with open(str(directory + '/variables.json'), 'w') as doc_json:
             json.dump(str(variables), doc_json)
 
-        with open(str(directory + '/data_inicial.json'), 'w') as doc_json:
-            self.data_inicial.to_json(doc_json, orient='table')
+        with open(str(directory + '/data_initial.json'), 'w') as doc_json:
+            self.data_initial.to_json(doc_json, orient='table')
 
         with open(str(directory + '/runned_list.json'), 'w') as doc_json:
             self.runned_list.to_json(doc_json, orient='table')
@@ -545,8 +711,8 @@ class denoise_functions:
         with open(str(directory + '/variables.json'), 'r') as doc_json:
             variables = literal_eval(json.load(doc_json))
 
-        with open(str(directory + '/data_inicial.json'), 'r') as doc_json:
-            self.data_inicial = pd.read_json(doc_json, orient='table')
+        with open(str(directory + '/data_initial.json'), 'r') as doc_json:
+            self.data_initial = pd.read_json(doc_json, orient='table')
 
         with open(str(directory + '/runned_list.json'), 'r') as doc_json:
             self.runned_list = pd.read_json(doc_json, orient='table')
@@ -592,8 +758,8 @@ class denoise_functions:
         with open(str(directory + '/variables.json'), 'r') as doc_json:
             variables = literal_eval(json.load(doc_json))
 
-        with open(str(directory + '/data_inicial.json'), 'r') as doc_json:
-            self.data_inicial = pd.read_json(doc_json, orient='table')
+        with open(str(directory + '/data_initial.json'), 'r') as doc_json:
+            self.data_initial = pd.read_json(doc_json, orient='table')
 
         with open(str(directory + '/runned_list.json'), 'r') as doc_json:
             self.runned_list = pd.read_json(doc_json, orient='table')
@@ -657,9 +823,9 @@ class denoise_functions:
         if not os.path.isdir(directory):
             os.mkdir(directory)
 
-        if not os.path.isfile(str(directory + '/data_inicial.json')):
-            with open(str(directory + '/data_inicial.json'), 'w') as doc_json:
-                self.data_inicial.to_json(doc_json, orient='table')
+        if not os.path.isfile(str(directory + '/data_initial.json')):
+            with open(str(directory + '/data_initial.json'), 'w') as doc_json:
+                self.data_initial.to_json(doc_json, orient='table')
         if not os.path.isfile(str(directory + '/runned_list.json')):
             with open(str(directory + '/runned_list.json'), 'w') as doc_json:
                 self.runned_list.to_json(doc_json, orient='table')
