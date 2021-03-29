b3344181d53059e7699632dd61c99173383d6cfd
AdriAntich <adriantich@gmail.com>
2021-03-29 23:09:48 +0200

    create motu bash

---
+++ src/add_lulu2MOTUfiles.R
@@ -0,0 +1,22 @@
+arg <- commandArgs(T)
+
+if (length(arg)<3) {
+  stop("EEEEEEPPPPPP!!!! falta alguna cosa")
+} else{
+  parent_motu <- arg[1]
+  data_lulu_input <- arg[2]
+  output_name <- arg[3]
+}
+
+
+data_lulu <- read.csv2(data_lulu_input)
+#parent_motu <- "PHY1_000003276"
+#output_name <- "PHY1_000003276_lulu"
+
+daughters_motu <- data_lulu$id_removed[grep(parent_motu,data_lulu$parent_id)]
+
+write.table(daughters_motu,file = output_name , quote = F,col.names = F,row.names = F)
+
+  
+  
+  
