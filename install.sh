adea2efc2e1946701a5bb360093e2e6bbc1d4a17
adriantich <adriantich@gmail.com>
2021-09-20 08:37:47 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # create required modules with python=>3.6
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv shell 3.6-dev
+pyenv virtualenv 3.6-dev DnoisE
+pyenv shell DnoisE3.6
 
 pip3 install pandas
 pip3 install tqdm
