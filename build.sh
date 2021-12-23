95026ebf5c2166352d6ba468af356793ed90029f
AdriAntich <adriantich@gmail.com>
2021-12-23 10:20:51 +0100

    meta.yaml and build.sh changed

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
 cd ./src
 
-python -m nuitka --enable-plugin=multiprocessing --follow-imports DnoisE.py
+python -m nuitka --enable-plugin=multiprocessing --static-libpython=no --follow-imports DnoisE.py
 
 
