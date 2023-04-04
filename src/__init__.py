f7a3499992b49bac98979101fad89d8720bebbe4
adriantich <adriantich@gmail.com>
2023-04-04 13:14:48 +0200

    modules added

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,11 @@
 import denoise_functions
 import DnoisE
 import entropy
+import get_entropy
 import import_data
 import running_denoise
+import test_DnoisE
 import transform_data
+import within_MOTU
 import write_output
 
